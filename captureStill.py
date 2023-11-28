6f1e593996d97bc848b81727f17778e31bba38b7
HoujieXiong <JeffX0506@gmail.com>
2023-11-28 15:20:26 -0500

    3:20PM

--- captureStill.py
+++ captureStill.py
@@ -4,8 +4,8 @@ from picamera2 import Picamera2, Preview
 #this will output a jepg file.
 picam2 = Picamera2()
 
-preview_config = picam2.create_preview_configuration(main={"size": (3280 , 2464)})
-picam2.configure(preview_config)
+#preview_config = picam2.create_preview_configuration(main={"size": (640 , 2464)})
+#picam2.configure(preview_config)
 
 #picam2.start_preview(Preview.QTGL)
 
