372782868bc244413ab41c76da3030023f2179c4
HoujieXiong <JeffX0506@gmail.com>
2023-11-30 14:23:12 -0500

    read QR code -v6

--- ReadQRCode.py
+++ ReadQRCode.py
@@ -10,12 +10,12 @@ while (True):
     # It's better to capture the still in this thread, not in the one driving the camera.
     #with open('test.jpg','rb') as image_file:
     image_file=Image.open("test.jpg")
-    image_file=image_file.convert('L')
+    greyScale=image_file.convert('L')
     image_file.close()
     #image_file.load()
     
     
-    codes=zbarlight.scan_codes(['qrcode'],image_file)
+    codes=zbarlight.scan_codes(['qrcode'],greyScale)
     print("found test.jpg")
     if(codes):
         print('QR codes: $s' % codes)
