8f359a585bca243c838907a1d40bced3feaa42af
HoujieXiong <JeffX0506@gmail.com>
2023-11-30 14:06:17 -0500

    read QR code

---
+++ ReadQRCode.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import time
+from PIL import Image
+import zbarlight 
+
+# Encode a VGA stream, and capture a higher resolution still image half way through.
+
+while (True):
+    # It's better to capture the still in this thread, not in the one driving the camera.
+    with open('test.jpg','rb') as image_file:
+        image_file=image_file.convert('L')
+        image_file.load()
+    
+    time.sleep(0.5)
+    codes=zbarlight.scan_codes(['qrcode'],image_file)
+    print('QR codes: $s' % codes)
+
+    f = open("test.txt", "w")
+    #f.write(codes)
+    f.close()
+
+    #open and read the file after the appending:
+    f = open("test.txt", "r")
+    print(f.read())
